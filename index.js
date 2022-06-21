c94ac4f23393b98b7de1f1526df6db549f08e675
Ethan Ratnofsky <ethanratnofsky@gmail.com>
2022-06-21 14:53:11 -0400

    Listening to Changes

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
 function createStore() {
     // The store should have four parts
-    // 1. The state
-    // 2. Get the state
-    // 3. Listen to changes on the state
-    // 4. Update the state
-    let state
 
+    // 1. The state
+    let state
+    let listeners = []
+
+    // 2. Get the state
     const getState = () => state
 
+    // 3. Listen to changes on the state
+    const subscribe = (listener) => {
+        listeners.push(listener)
+        return () => {
+            listeners = listeners.filter((l) => l !== listener)
+        }
+    }
+
+    // 4. Update the state
+
     return {
-        getState
+        getState,
+        subscribe,
     }
 }
